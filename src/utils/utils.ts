f229ab036cef0a97c8affe2cab5773eadf9ecd9a
Jose <romerojosemanuel@hotmail.com>
2024-05-28 17:53:25 -0600

    add a max-width to Tag list

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 
 // returns the date as a string "day/month/year"
 export const formattedDate = (pubDate: any, month = 'numeric') => {
-	const options = { year: 'numeric', month: month, day: 'numeric' }
+	const options = { year: 'numeric', month: month, day: 'numeric', timeZone: "UTC" }
 	return pubDate.toLocaleDateString('es', options)
 }
